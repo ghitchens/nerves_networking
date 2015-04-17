98327b142c3de1c6adc85cd449df52a47367dcd9
Chris Dutton <chris@rosepointnav.com>
2015-04-16 22:41:44 -0700

    Updated to VERSION file

--- mix.exs
+++ mix.exs
@@ -4,13 +4,22 @@ defmodule Ethernet.Mixfile do
 
   def project, do: [
      app: :ethernet,
-     version: "0.1.0",
+     version: version,
      elixir: "~> 1.0",
      deps: deps
   ]
 
   def application, do: [applications: [:logger]]
 
-  defp deps, do: []
+  defp deps, do: [
+    {:earmark, "~> 0.1", only: :dev},
+    {:ex_doc, "~> 0.7", only: :dev}
+  ]
 
+  defp version do
+    case File.read("VERSION") do
+      {:ok, ver} -> String.strip ver
+      _ -> "0.0.0-dev"
+    end
+  end
 end
