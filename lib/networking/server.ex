dfe06c57ae10212f9a44e25cdf575ed8e862d86c
Garth Hitchens <ghitchens@me.com>
2016-07-26 22:58:32 -0700

    modify dhcpdiscover timeouts

--- lib/networking/server.ex
+++ lib/networking/server.ex
@@ -80,7 +80,7 @@ defmodule Nerves.Networking.Server do
 
   # retry after 10 seconds for the first 10 retries, then 1 min
   defp dhcp_retry_interval(tries) when tries >= 10, do: 60000
-  defp dhcp_retry_interval(_tries), do: 10000
+  defp dhcp_retry_interval(_tries), do: 15000
 
   # update changes and announce, returning new state
   defp update_and_announce(state, changes) do
@@ -111,7 +111,7 @@ defmodule Nerves.Networking.Server do
   end
 
   defp raw_dhcp_request(state) do
-    Subsystem.dhcp_request(state.interface, state[:hostname])
+    Subsystem.dhcp_request(state.interface, state[:hostname], state.dhcp_retries)
   end
 
   defp configure_dhcp(state, params) do
